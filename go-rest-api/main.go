386fc9521c9b73d8e5e04c253d9c727b291e6946
Robinson <robinsonvs@gmail.com>
2023-06-16 01:40:56 -0300

    create a first router

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome")
+}
+
 
 func main() {
-	fmt.Println("Hello")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
