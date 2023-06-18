97632e1988b5d725425d4b63cf401c283c593ece
Robinson <robinsonvs@gmail.com>
2023-06-17 22:16:33 -0300

    add new routers

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -102,5 +102,9 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	router.HandleFunc("/event", createEvent).Methods("POST")
+	router.HandleFunc("/events", getAllEvents).Methods("GET")
+	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
+	router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
+	router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
